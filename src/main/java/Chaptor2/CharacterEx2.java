49db3b215230e45efdeda9cf54a9f0d3702ca668
Luke <github luke280705@gmail.com>
2023-03-14 20:30:59 +0900

    Add

---
+++ src/main/java/Chaptor2/CharacterEx2.java
@@ -0,0 +1,13 @@
+package Chaptor2;
+
+public class CharacterEx2
+{
+    public static void main(String[] args)
+    {
+        char ch1 = '\uc774';
+        char ch2 = '\uc778';
+
+        System.out.print(ch1);
+        System.out.print(ch2);
+    }
+}
